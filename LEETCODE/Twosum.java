d49654a73f9d343263870ebdea27645e8420a2a3
yokeshwaran <yokeshwaran032005@gmail.com>
2025-12-16 14:13:02 +0530

    hi

--- LEETCODE/Twosum.java
+++ LEETCODE/Twosum.java
@@ -1,4 +1,8 @@
-class Solution {
+
+public class twosum {
+    public static void main(String[] nums) {
+        
+    
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
@@ -9,4 +13,5 @@ class Solution {
         }
         return new int[]{-1,-1};
     }
+}
 }
